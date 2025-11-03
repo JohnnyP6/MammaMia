530936a725f05baba0b6daa3b387efba15e08894
JohnnyP6 <jonathan.pena.g93@gmail.com>
2025-11-03 10:29:51 -0300

    Hito 2 - Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar';
-/* import Home from './components/Home'; */
+import Home from './components/Home';
 import Footer from './components/Footer';
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from './pages/LoginPage';
